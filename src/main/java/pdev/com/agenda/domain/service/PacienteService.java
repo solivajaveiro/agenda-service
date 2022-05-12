76d2006acd5d6f14083d0239af78b4025ab097a8
Michel Luis da Silva Soliva <msoliva@mcity220.aquarius.cpqd.com.br>
2022-05-12 02:27:34 -0300

    Created Service

---
+++ src/main/java/pdev/com/agenda/domain/service/PacienteService.java
@@ -0,0 +1,37 @@
+package pdev.com.agenda.domain.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import pdev.com.agenda.domain.entity.Paciente;
+import pdev.com.agenda.domain.repository.PacienteRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class PacienteService {
+
+    private final PacienteRepository pacienteRepository;
+
+    public Paciente salvar(Paciente paciente) {
+
+        //TODO: para validar se o cpf já existe
+
+        return pacienteRepository.save(paciente);
+    }
+
+    public List<Paciente> listarTodos() {
+        return pacienteRepository.findAll();
+    }
+
+    public Optional<Paciente> buscarPorId(Long id) {
+        return pacienteRepository.findById(id);
+    }
+
+    public void deletar(Long id) {
+        pacienteRepository.deleteById(id);
+    }
+}
